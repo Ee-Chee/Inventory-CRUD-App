dc0e4107ddc625a7fa820bb4692ae669b41cc7a3
Ee-Chee <eechee.leng@gmail.com>
2020-06-26 18:44:29 +0200

    ready for live production

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
+console.log(__dirname);
+
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
