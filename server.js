8b265273994350910d9d422d826b1e0288a5bb41
Ee-Chee <eechee.leng@gmail.com>
2020-06-26 02:40:26 +0200

    ready for live production

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/src'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/src/index.html'));
 });
 
 app.listen(process.env.PORT || 8081);
