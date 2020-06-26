43e5ccbf860276fd1c9d48956ff2f4929a40a695
Ee-Chee <eechee.leng@gmail.com>
2020-06-26 02:12:28 +0200

    ready for live production

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/angular-8-crud-app'));
+app.use(express.static(__dirname + '/dist/'));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-8-crud-app/index.html'));
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/angular-8-crud-app/index.html'));
+// });
 
 app.listen(process.env.PORT || 8081);
