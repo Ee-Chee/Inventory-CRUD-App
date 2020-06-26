767c3d0ca48039e9a7d55137bc9cfe3e38e196ca
Ee-Chee <eechee.leng@gmail.com>
2020-06-26 02:08:04 +0200

    ready for live production

---
+++ server.js
@@ -0,0 +1,12 @@
+//for Heroku live production to server angular app without api server
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/angular-8-crud-app'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/angular-8-crud-app/index.html'));
+});
+
+app.listen(process.env.PORT || 8081);
